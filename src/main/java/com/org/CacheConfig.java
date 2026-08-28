49390ffe31c8fcd5d2e80defc98562d65ac1909e
saurav kumar <sauravkumar@sauravs-MacBook-Air.local>
2026-08-28 21:05:09 +0530

    red

--- src/main/java/com/org/CacheConfig.java
+++ src/main/java/com/org/CacheConfig.java
@@ -14,10 +14,8 @@ import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 
 
-import org.springframework.data.redis.serializer.GenericJacksonJsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
-import tools.jackson.databind.ObjectMapper;
+import org.springframework.data.redis.serializer.RedisSerializer;
 
 
 @Configuration
@@ -26,22 +24,18 @@ public class CacheConfig {
 
     @Bean
     public RedisCacheManager cacheManager(
-            RedisConnectionFactory connectionFactory,
-            ObjectMapper objectMapper) {
-
-        GenericJacksonJsonRedisSerializer jsonSerializer =
-                new GenericJacksonJsonRedisSerializer(objectMapper);
+            RedisConnectionFactory connectionFactory) {
 
         RedisCacheConfiguration cacheConfiguration =
                 RedisCacheConfiguration.defaultCacheConfig()
                         .entryTtl(Duration.ofMinutes(10))
                         .serializeKeysWith(
                                 RedisSerializationContext.SerializationPair
-                                        .fromSerializer(new StringRedisSerializer())
+                                        .fromSerializer(RedisSerializer.string())
                         )
                         .serializeValuesWith(
                                 RedisSerializationContext.SerializationPair
-                                        .fromSerializer(jsonSerializer)
+                                        .fromSerializer(RedisSerializer.json())
                         );
 
         return RedisCacheManager.builder(connectionFactory)
