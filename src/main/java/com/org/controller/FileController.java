db7e627e6f24573715fe797018717c5b4ea66bf1
saurav kumar <sauravkumar@sauravs-MacBook-Air.local>
2026-08-28 18:51:03 +0530

    minor_changes

--- src/main/java/com/org/controller/FileController.java
+++ src/main/java/com/org/controller/FileController.java
@@ -1,11 +1,14 @@
 package com.org.controller;
 import java.io.IOException;
 
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.org.service.S3StorageService;
+import software.amazon.awssdk.core.ResponseBytes;
+import software.amazon.awssdk.services.s3.model.GetObjectResponse;
 
 @RestController
 @RequestMapping("/v1/files")
@@ -24,5 +27,58 @@ public class FileController {
         String key = s3StorageService.upload(file);
 
         return ResponseEntity.ok(key);
+    }
+
+    @GetMapping("/download")
+    public ResponseEntity<byte[]> download(
+            @RequestParam("key") String key) {
+
+        ResponseBytes<GetObjectResponse> object =
+                s3StorageService.download(key);
+
+        String contentType =
+                object.response().contentType() != null
+                        ? object.response().contentType()
+                        : "application/octet-stream";
+
+        return ResponseEntity.ok()
+                .header(
+                        "Content-Disposition",
+                        "attachment; filename=\"" +
+                                key.substring(key.lastIndexOf("/") + 1) +
+                                "\""
+                )
+                .contentType(MediaType.parseMediaType(contentType))
+                .body(object.asByteArray());
+    }
+
+
+    @GetMapping("/upload-url")
+    public ResponseEntity<String> getUploadUrl(
+            @RequestParam("fileName") String fileName,
+            @RequestParam("contentType") String contentType) {
+
+        String key =
+                "employees/" +
+                        System.currentTimeMillis() +
+                        "-" +
+                        fileName;
+
+        return ResponseEntity.ok(
+                s3StorageService.generateUploadUrl(
+                        key,
+                        contentType
+                )
+        );
+    }
+
+
+    @GetMapping("/download-url")
+    public ResponseEntity<String> getDownloadUrl(
+            @RequestParam("key") String key) {
+
+        return ResponseEntity.ok(
+                s3StorageService.generateDownloadUrl(key)
+        );
     }
 }
--- src/main/java/com/org/service/S3StorageService.java
+++ src/main/java/com/org/service/S3StorageService.java
@@ -9,18 +9,32 @@ import org.springframework.web.multipart.MultipartFile;
 import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import software.amazon.awssdk.core.ResponseBytes;
+import software.amazon.awssdk.services.s3.model.GetObjectRequest;
+import software.amazon.awssdk.services.s3.model.GetObjectResponse;
+import java.time.Duration;
+
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
+import software.amazon.awssdk.services.s3.presigner.model.GetObjectPresignRequest;
+import software.amazon.awssdk.services.s3.presigner.model.PutObjectPresignRequest;
 
 @Service
 public class S3StorageService {
 
     private static final String BUCKET_NAME =
             "employee-service-files-196253397204";
-
     private final S3Client s3Client;
+    private final S3Presigner s3Presigner;
 
-    public S3StorageService(S3Client s3Client) {
+    public S3StorageService(
+            S3Client s3Client,
+            S3Presigner s3Presigner) {
+
         this.s3Client = s3Client;
+        this.s3Presigner = s3Presigner;
     }
+
 
     public String upload(MultipartFile file) throws IOException {
 
@@ -39,5 +53,63 @@ public class S3StorageService {
         );
 
         return key;
+    }
+
+
+
+
+    public ResponseBytes<GetObjectResponse> download(String key) {
+
+        GetObjectRequest request = GetObjectRequest.builder()
+                .bucket(BUCKET_NAME)
+                .key(key)
+                .build();
+
+        return s3Client.getObjectAsBytes(request);
+    }
+
+
+    public String generateUploadUrl(
+            String key,
+            String contentType) {
+
+        PutObjectRequest putObjectRequest =
+                PutObjectRequest.builder()
+                        .bucket(BUCKET_NAME)
+                        .key(key)
+                        .contentType(contentType)
+                        .build();
+
+        PutObjectPresignRequest presignRequest =
+                PutObjectPresignRequest.builder()
+                        .signatureDuration(Duration.ofMinutes(10))
+                        .putObjectRequest(putObjectRequest)
+                        .build();
+
+        return s3Presigner
+                .presignPutObject(presignRequest)
+                .url()
+                .toString();
+    }
+
+
+    public String generateDownloadUrl(String key) {
+
+        GetObjectRequest getObjectRequest =
+                GetObjectRequest.builder()
+                        .bucket(BUCKET_NAME)
+                        .key(key)
+                        .build();
+
+        GetObjectPresignRequest presignRequest =
+                GetObjectPresignRequest.builder()
+                        .signatureDuration(Duration.ofMinutes(10))
+                        .getObjectRequest(getObjectRequest)
+                        .build();
+
+        return s3Presigner
+                .presignGetObject(presignRequest)
+                .url()
+                .toString();
     }
 }
